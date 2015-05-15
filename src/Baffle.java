5c228ef92175e030a9e67defaefd3b5a605fe5b7
Chathan <cdriehuys@gmail.com>
2015-05-14 23:55:34 -0400

    Adding documentation.

--- src/Baffle.java
+++ src/Baffle.java
@@ -8,16 +8,24 @@ public class Baffle extends ClickableArea {
     private boolean hasBaffle, forwardBaffle, baffleVisible, flagged;
 
 
+    /** Default constructor **/
     public Baffle() {
 
+        // initialize all booleans to false
         hasBaffle = forwardBaffle = baffleVisible = flagged = false;
     }
 
     /************************** Overridden Methods **************************/
 
+    /**
+     * Given an incoming beam's direction, find which direction it will exit the baffle in.
+     * @param enterDir the beam's initial direction
+     * @return the beam's new direction
+     */
     @Override
     public short getBeamDir(short enterDir) {
 
+        // determine if entry direction is valid
         if (enterDir != BaffleGrid.BEAM_DOWN
                 && enterDir != BaffleGrid.BEAM_LEFT
                 && enterDir != BaffleGrid.BEAM_RIGHT
@@ -26,8 +34,10 @@ public class Baffle extends ClickableArea {
             throw new IllegalArgumentException("enterDir must be one of the predefined BaffleGrid.BEAM_--- constants.");
         }
 
+        // determine if there is a baffle to bounce off of
         if (hasBaffle) {
 
+            // reflection logic
             if (forwardBaffle) {
 
                 switch (enterDir) {
@@ -63,42 +73,58 @@ public class Baffle extends ClickableArea {
 
         } else {
 
+            // no baffle, return original direction
             return enterDir;
         }
     }
 
+    /**
+     * Draw the baffle to the screen.
+     * @param g the graphics object to draw on
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // if the box has been flagged, draw a flag
         if (flagged) {
             g.drawImage(FLAG, 0, 0, getWidth(), getHeight(), null);
         }
 
+        // if the box has a baffle and it's been discovered, draw the baffle
         if (hasBaffle && baffleVisible) {
             g.setColor(Color.BLACK);
             g.drawLine(0, forwardBaffle ? getHeight() : 0, getWidth(), forwardBaffle ? 0 : getHeight());
         }
     }
 
+    /**
+     * The function that is called when the box is left-clicked
+     */
     @Override
     public void onLeftClick() {
 
+        // if it's not flagged
         if (!flagged) {
-            super.onLeftClick();
+            super.onLeftClick();    // parent's left-click function
 
+            // if box has a baffle
             if (hasBaffle) {
-                baffleVisible = true;
-                getBaffleGrid().getGame().getScorePanel().addScore(Game.BAFFLE_PRIZE);
-                getBaffleGrid().getGame().getScorePanel().foundBaffle();
+                baffleVisible = true;   // make the baffle visible
+                getBaffleGrid().getGame().getScorePanel().foundBaffle();    // add to score
             } else {
-                getBaffleGrid().getGame().getScorePanel().addScore(Game.WRONG_GUESS);
+                getBaffleGrid().getGame().getScorePanel().addScore(Game.WRONG_GUESS);   // wrong guess, subtract score
             }
         }
     }
 
+    /**
+     * The function that is called when the box is right-clicked
+     */
     @Override
     public void onRightClick() {
+
+        // toggle the flag
         flagged = !flagged;
     }
 
@@ -109,6 +135,9 @@ public class Baffle extends ClickableArea {
 
     /**************************** Other Methods *****************************/
 
+    /**
+     * Generates a baffle with a random direction.
+     */
     public void generateBaffle() {
 
         hasBaffle = true;

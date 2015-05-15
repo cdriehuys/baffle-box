4a238446860ef20cf73a3f52e5f411a4bfca7041
Chathan <cdriehuys@gmail.com>
2015-05-15 10:03:22 -0400

    Adding documentation.

--- src/Game.java
+++ src/Game.java
@@ -35,40 +35,53 @@ public class Game extends JPanel {
         // set the preferred size of the panel
         setPreferredSize(new Dimension(width, height));
 
+        // create a new border layout
         setLayout(new BorderLayout());
 
+        // connect to the highscores database
         scoreDB = new HighscoreDB();
 
+        // initialize the interface
         initialize();
 
+        // create a timer to repaint every 20ms
         Timer drawTimer = new Timer(20, new DrawTimerListener());
         drawTimer.start();
     }
 
     /************************** Overridden Methods **************************/
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-    }
-
     /****************************** Accessors *******************************/
+
+    /**
+     * Accessor for the Game's ScorePanel.
+     * @return the Game's ScorePanel
+     */
+    public ScorePanel getScorePanel() { return scorePanel; }
+
 
     /****************************** Mutators ********************************/
 
     /**************************** Other Methods *****************************/
 
-    public ScorePanel getScorePanel() { return scorePanel; }
-
+    /**
+     * Shows the history dialog.
+     */
     public void showHistory() {
-        if (!history.isVisible())
-            history.setVisible(true);
+        history.setVisible(true);
     }
 
+    /**
+     * Method executed when the game is won.
+     */
     public void win() {
 
+        // show the high scores
         doHighScores();
 
+        /* The rest of this method creates a new dialog telling the user that they won. It gives the user
+         * the option to quit or play again.
+         */
         final JDialog dialog = new JDialog();
 
         JPanel panel = new JPanel();
@@ -125,28 +138,42 @@ public class Game extends JPanel {
         dialog.setVisible(true);
     }
 
+    /**
+     * Logs a move to the Game's HistoryDialog.
+     * @param move the move to log
+     */
     public void logMove(String move) {
         history.logMove(move);
     }
 
+    /**
+     * Creates a dialog prompting the user to choose a difficulty level.
+     * @return the difficulty level. Must be one of <code>Game.EASY</code>, <code>Game.MEDIUM</code>,
+     *         or <code>Game.HARD</code>.
+     */
     private int getDifficulty() {
 
+        // create new panel
         JPanel panel = new JPanel();
 
+        // create radio buttons for different difficulties
         JRadioButton easy = new JRadioButton("Easy", false);
         JRadioButton med = new JRadioButton("Medium");
         JRadioButton hard = new JRadioButton("Hard");
 
+        // add the radio buttons to a group and set the default option to medium
         ButtonGroup radios = new ButtonGroup();
         radios.add(easy);
         radios.add(med);
         radios.add(hard);
         med.setSelected(true);
 
+        // add the buttons to the panel
         panel.add(easy);
         panel.add(med);
         panel.add(hard);
 
+        // get the results from a JOptionPane with the custom panel
         int res = JOptionPane.showConfirmDialog(this, panel, "Difficulty", JOptionPane.DEFAULT_OPTION);
         if (res == JOptionPane.OK_OPTION) {
 
@@ -160,15 +187,21 @@ public class Game extends JPanel {
             System.exit(0);
         }
 
-        return 0;
+        // if we somehow get to this point, default to medium difficulty
+        return Game.MEDIUM;
     }
 
+    /**
+     * Initializes the Game.
+     */
     private void initialize() {
 
+        // get the difficulty
         gameMode = getDifficulty();
 
         int numBaffles;
 
+        // determine how many baffles to create based on the game difficulty
         switch (gameMode) {
             case EASY:
                 numBaffles = EASY_BAFFLES;
@@ -182,32 +215,48 @@ public class Game extends JPanel {
                 numBaffles = MEDIUM_BAFFLES;
         }
 
+        // create a new 10x10 baffle grid with the previously specified number of baffles
         add(new BaffleGrid(10, 10, numBaffles), BorderLayout.CENTER);
 
+        // create and add a new ScorePanel
         scorePanel = new ScorePanel(numBaffles);
         add(scorePanel, BorderLayout.NORTH);
 
+        // make a new HistoryDialog
         history = new HistoryDialog(null);
 
+        // revalidate so that all components are correctly rendered
         revalidate();
     }
 
+    /**
+     * Method used to clean up a game after winning and before restarting.
+     */
     private void destroy() {
 
+        // remove all components
         removeAll();
 
+        // destroy the score panel
         scorePanel = null;
 
+        // destroy the history
         history.dispose();
         history = null;
 
+        // make sure none of the removed components are rendered
         revalidate();
     }
 
+    /**
+     * Handles displaying of the high scores.
+     */
     private void doHighScores() {
 
+        // get the user's name
         String name = JOptionPane.showInputDialog(this, "Enter your name", "Enter Name", JOptionPane.QUESTION_MESSAGE);
 
+        // add the score to the score database and display the scores
         if (name != null) {
             scoreDB.addScore(name, scorePanel.getScore(), gameMode);
             new HighscoreDialog(null, gameMode);
@@ -216,8 +265,13 @@ public class Game extends JPanel {
 
     /**************************** Helper Classes ****************************/
 
+    /* Helper class for the Game that listens for timer events and repaints the game */
     private class DrawTimerListener implements ActionListener {
 
+        /**
+         * Method called when an ActionEvent is received from the timer.
+         * @param e the ActionEvent received
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
             repaint();

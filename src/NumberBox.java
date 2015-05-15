49456ff4e9f59e5208c8ad8d9a1ce956940ef89d
Chathan <cdriehuys@gmail.com>
2015-05-15 10:39:04 -0400

    Adding documentation.

--- src/NumberBox.java
+++ src/NumberBox.java
@@ -11,6 +11,10 @@ public class NumberBox extends ClickableArea {
     private int val;
 
 
+    /**
+     * Create a new <code>NumberBox</code> with the given value.
+     * @param val the value to display
+     */
     public NumberBox(int val) {
         super();
 
@@ -22,10 +26,15 @@ public class NumberBox extends ClickableArea {
     }
     /************************** Overridden Methods **************************/
 
+    /**
+     * Paints the object onto the given graphics object
+     * @param g the graphics object to draw on
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        // change font if highlighted
         if (highlighted) {
             g.setColor(isEnd ? END_COLOR : START_COLOR);
             g.setFont(g.getFont().deriveFont(Font.BOLD));
@@ -34,9 +43,13 @@ public class NumberBox extends ClickableArea {
             g.setFont(g.getFont().deriveFont(Font.PLAIN));
         }
 
+        // draw the NumberBox's value
         g.drawString(String.valueOf(val), getWidth() / 3, getHeight() / 2);
     }
 
+    /**
+     * Fire a beam from this box.
+     */
     @Override
     public void onLeftClick() {
         getBaffleGrid().getGame().getScorePanel().addScore(Game.FIRE_PENALTY);
@@ -45,12 +58,24 @@ public class NumberBox extends ClickableArea {
 
     /****************************** Accessors *******************************/
 
+    /**
+     * Get this box's value.
+     * @return the box's value
+     */
     public int getVal() { return val; }
 
     /****************************** Mutators ********************************/
 
+    /**
+     * Clear the highlighting on the number box.
+     */
     public void clearHighlight() { highlighted = false; }
 
+    /**
+     * Highlight the <code>NumberBox</code>.
+     * @param val if the box should be highlighted
+     * @param isEnd determines if the box should be highlighted with the beam end color or the beam start color
+     */
     public void setHighlighted(boolean val, boolean isEnd) {
         highlighted = val;
         this.isEnd = isEnd;

6d994f63976f4fdfaa3d07221a7cc335d1d8e5f9
Chathan <cdriehuys@gmail.com>
2015-05-15 10:46:27 -0400

    Adding documentation.

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -8,15 +8,21 @@ public class ScorePanel extends JPanel {
 
     private JLabel scoreLabel, bafflesLeftLabel;
 
-
+    /**
+     * Create a new score panel.
+     * @param numBaffles the initial value for baffles remaining.
+     */
     public ScorePanel(int numBaffles) {
 
+        // initialize score and remaining baffles
         score = 0;
         remainingBaffles = numBaffles;
 
+        // make labels for the score and baffles remaining.
         scoreLabel = new JLabel("Score: " + score);
         bafflesLeftLabel = new JLabel("Remaining Baffles: " + remainingBaffles);
 
+        // history button to show log of past moves
         JButton historyBtn = new JButton("History");
         historyBtn.addActionListener(new ActionListener() {
             @Override
@@ -25,6 +31,7 @@ public class ScorePanel extends JPanel {
             }
         });
 
+        // add elements to panel
         add(scoreLabel);
         add(bafflesLeftLabel);
         add(historyBtn);
@@ -34,12 +41,24 @@ public class ScorePanel extends JPanel {
 
     /****************************** Accessors *******************************/
 
+    /**
+     * Get the parent <code>Game</code> object.
+     * @return the parent <code>Game</code> object
+     */
     public Game getGame() { return (Game)getParent(); }
 
+    /**
+     * Get the current score.
+     * @return the current score
+     */
     public int getScore() { return score; }
 
     /****************************** Mutators ********************************/
 
+    /**
+     * Called when a baffle is found. Adds to score and subtracts from remaining baffles. Also
+     * determines if the game has been won.
+     */
     public void foundBaffle() {
         remainingBaffles--;
         score += Game.BAFFLE_PRIZE;
@@ -48,6 +67,10 @@ public class ScorePanel extends JPanel {
             getGame().win();
     }
 
+    /**
+     * Add to the score.
+     * @param amount the amount to add to the score
+     */
     public void addScore(int amount) {
         score += amount;
         scoreLabel.setText("Score: " + score);
